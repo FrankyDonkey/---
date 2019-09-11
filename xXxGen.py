1f22c500bbdfcc4d17e865ecd2e407f0b1df850b
Franky Donkey <53109529+FrankyDonkey@users.noreply.github.com>
2019-09-11 15:17:58 -0400

    Create xXxGen.py

---
+++ xXxGen.py
@@ -0,0 +1,37 @@
+# $cript By Priv8Ghost
+# xXx Generator A Fresh IBAN xXx
+# Group: facebook.com/groups/pegMM
+# Thanks To FrankyDonkey
+print('''
+______     _      _____ _____ _               _   
+| ___ \   (_)    |  _  |  __ \ |             | |  
+| |_/ / __ ___   _\ V /| |  \/ |__   ___  ___| |_ 
+|  __/ '__| \ \ / / _ \| | __| '_ \ / _ \/ __| __|
+| |  | |  | |\ V / |_| | |_\ \ | | | (_) \__ \ |_ 
+\_|  |_|  |_| \_/\_____/\____/_| |_|\___/|___/\__|
+######### xXx Generator A Fresh IBAN xXx #########
+''')
+import random
+x = input("PUT ONLY BLZ OF THE TARGET BANK:")
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+print("DE",random.randint(0,9),random.randint(0,9),x,random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9),sep='')
+## https://frankydonkey.github.io/-
+## Goodbye Friend!
